493a2952ce2fd41a35b01cf16584c6e1b9b0cd95
FannarHrafn <fannarhrafn@gmail.com>
2018-05-13 12:42:24 +0000

    innerhtml
    Shielder virkar og það er bara html sem sækir js en maze.html fær bara 404 þegar það reynir að sækja sitt js þótt að slóðirnar sé alveg réttar

--- loka/lokaverkefni.py
+++ loka/lokaverkefni.py
@@ -2,21 +2,28 @@
 #lokaverkefni vor 2018
 from bottle import template,route,run,static_file
 import json
-with open("json/gogn.json","r") as skra:
+with open("static/gogn.json","r") as skra:
     gogn = json.load(skra)
 skra.close()
-print(gogn['data'][0]['img'])
+innerhtml="<div>\n"
 for i in gogn['data']:
-    print(i['img'])
+    print(i['tags'])
+    line = "<a data-tags='"+i['tags']+"' href="+i['link']+" > <img src='/static/"+i['img']+"' alt='"+i['nafn']+"'></a>\n"
+    innerhtml += line
+innerhtml += "</div>"
+print(innerhtml)
 @route('/')
 def index():
-        return template('index.tpl',gogn=gogn)
-#bendi á static skráina og að allt í henni sé static
-@route('/maze')
+        return template('views/index.tpl',lines=innerhtml)
+@route('/defence/shielder.html')
+def shielder():
+    return template('defence/shielder.html')
+@route('/maze/www/maze.html')
 def maze():
-    return
+    return template('maze/www/maze.html')
+'''
 @route('/static/<filename>')
-def static(filename):
+def server_static(filename):
     return static_file(filename, root='./static')
-
+'''
 run()
